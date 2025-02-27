6aaf0044e7b1cf15ff70940aa940d5dcc66da690
uuganaa uuganbayar <uuganaauuganbayar@Mac.lan>
2025-02-27 15:09:17 +0800

    fix builder

--- app/_components/sections/YoutubeSection.tsx
+++ app/_components/sections/YoutubeSection.tsx
@@ -2,11 +2,15 @@ import { Section } from "@/types/sections";
 import React from "react";
 
 const YoutubeSection = ({ section }: { section: Section }) => {
+  const embedUrl = section.config.videoUrl.replace("watch?v=", "embed/");
   return (
     <section className="relative h-[600px]">
       <iframe
-        className="w-full ratio-16/9 h-full"
-        src={section.config.videoUrl}
+        className="w-full h-full"
+        src={embedUrl}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
       ></iframe>
     </section>
   );
