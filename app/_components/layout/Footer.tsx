c97d58809014158b223e93146e07c304ae840075
uuganbayar.b <uuganaa1333@gmail.com>
2025-03-10 18:09:33 +0800

    added footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h4 className="text-lg font-semibold mb-2">Quick Links</h4>
             <ul>
-              {cpDetail.menus.footer.map((menu: any) => (
+              {cpDetail.menus.footerMenu.map((menu: any) => (
                 <li key={menu._id}>
                   <Link href={menu.url} className="hover:underline">
                     {menu.label}
@@ -27,51 +27,63 @@ export default function Footer() {
           </div>
           <div className="w-full md:w-1/3">
             <h4 className="text-lg font-semibold mb-2">Contact Us</h4>
-            <p>Email: {cpDetail?.externalLinks?.emails[0]}</p>
-            <p>Phone: {cpDetail?.externalLinks?.phones[0]}</p>
-            <p>Address: {cpDetail?.externalLinks?.address} </p>
+            <p>Email: {cpDetail?.additional?.social.find((item) => item.name === "emails")?.url[0]}</p>
+            <p>Phone: {cpDetail?.additional?.social.find((item) => item.name === "phones")?.url[0]}</p>
+            <p>Address: {cpDetail?.additional?.social.find((item) => item.name === "address")?.url} </p>
             <div className="flex space-x-4">
-              {cpDetail?.externalLinks?.facebook && (
-                <a href={cpDetail?.externalLinks?.facebook} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <Facebook />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "facebook")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "facebook")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <Facebook />
                 </a>
               )}
-              {cpDetail?.externalLinks?.twitter && (
-                <a href={cpDetail?.externalLinks?.twitter} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <Twitter />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "twitter")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "twitter")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <Twitter />
                 </a>
               )}
-              {cpDetail?.externalLinks?.linkedin && (
-                <a href={cpDetail?.externalLinks?.linkedin} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <Linkedin />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "linkedin")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "linkedin")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <Linkedin />
                 </a>
               )}
-              {cpDetail?.externalLinks?.youtube && (
-                <a href={cpDetail?.externalLinks?.youtube} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <Youtube />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "youtube")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "youtube")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <Youtube />
                 </a>
               )}
-              {cpDetail?.externalLinks?.instagram && (
-                <a href={cpDetail?.externalLinks?.instagram} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <Instagram />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "instagram")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "instagram")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <Instagram />
                 </a>
               )}
-              {cpDetail?.externalLinks?.whatsapp && (
-                <a href={cpDetail?.externalLinks?.whatsapp} className="text-white hover:text-gray-300 transition-colors duration-200">
-                  {" "}
-                  <MessageCircle />{" "}
+              {cpDetail?.additional?.social.find((item) => item.name === "whatsapp")?.url && (
+                <a
+                  href={cpDetail?.additional?.social.find((item) => item.name === "whatsapp")?.url as string}
+                  className="text-white hover:text-gray-300 transition-colors duration-200"
+                >
+                  <MessageCircle />
                 </a>
               )}
             </div>
           </div>
         </div>
         <div className="mt-8 text-center">
-          <p>{cpDetail?.copyright}</p>
+          <p>{cpDetail?.additional?.copyright?.text}</p>
         </div>
       </div>
     </footer>
