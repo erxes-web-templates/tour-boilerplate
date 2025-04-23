70ba404ec4a9b23145629efd17c272354692cf6a
uuganaa uuganbayar <uuganaauuganbayar@uuganaas-MacBook-Pro.local>
2025-04-23 13:24:38 +0800

    added messenger

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,21 @@ import Header from "./_components/layout/Header";
 import Footer from "./_components/layout/Footer";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
 import data from "../data/configs.json";
-import { getFileUrl } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
+const baseUrl = new URL(process.env.ERXES_API_URL as string).origin;
 
 export const metadata: Metadata = {
   title: data.meta.title,
   description: data.meta.description,
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
@@ -23,6 +28,30 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {/* <meta property="og:image" content={getFileUrl(data.meta.logo)} /> */}
       </head>
       <body className={inter.className}>
+        {data?.additional?.integrations?.messengerId && (
+          <Script
+            id="erxes"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+    window.erxesSettings = {
+      messenger: {
+        brand_id: "${data?.additional?.integrations?.messengerId}",
+      },
+    };
+    
+    (() => {
+      const script = document.createElement('script');
+      script.src = "${baseUrl}/widgets/build/messengerWidget.bundle.js";
+      script.async = true;
+
+      const entry = document.getElementsByTagName('script')[0];
+      entry.parentNode.insertBefore(script, entry);
+    })();
+  `,
+            }}
+          />
+        )}
         <ApolloWrapper>
           <Header />
           <main>{children}</main>
