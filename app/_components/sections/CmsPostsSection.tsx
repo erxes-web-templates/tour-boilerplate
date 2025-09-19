fa48ecfba69f0b97f4ab4eab86ea16159155595e
irmum <dev@erxes.io>
2025-09-19 14:54:01 +0800

    debug log

--- app/_components/sections/CmsPostsSection.tsx
+++ app/_components/sections/CmsPostsSection.tsx
@@ -17,8 +17,10 @@ const CmsPostsSection = async ({ section }: { section: Section }) => {
   });
 
   console.log(posts, "menu in posts");
+  console.log("aaaaa")
   return (
     <section className="py-16 bg-gray-100">
+      aaa
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">{section.config.title}</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
