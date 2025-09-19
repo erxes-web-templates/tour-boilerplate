6ee9c34e932880a6a35d746f363e6da82b647b3b
irmum <dev@erxes.io>
2025-09-19 13:21:39 +0800

    cms

--- app/_components/sections/CmsPostsSection.tsx
+++ app/_components/sections/CmsPostsSection.tsx
@@ -22,7 +22,7 @@ const CmsPostsSection = async ({ section }: { section: Section }) => {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">{section.config.title}</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {/* {posts.map((post: CmsPost) => (
+          {posts.map((post: CmsPost) => (
         <Card key={post._id}>
           <CardHeader>
             {post.thumbnail && <Image src={getFileUrl(post.thumbnail.url)} alt={post.title} width={300} height={300} className="rounded-t-lg" />}
@@ -39,7 +39,7 @@ const CmsPostsSection = async ({ section }: { section: Section }) => {
             </Link>
           </CardFooter>
         </Card>
-      ))} */}
+      ))}
         </div>
         <div className=" text-center mt-6 ">
           <Link className="underline" href={"/blog"}>
